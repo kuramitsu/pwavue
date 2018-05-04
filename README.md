9d65f97dc7f6941185800ac61fc9d7d254f53d5b
takuma <takuma@tapu.local>
2018-05-04 16:44:30 +0900

    typescript support

--- README.md
+++ README.md
@@ -4,3 +4,14 @@ pwa &amp; vue &amp; heroku
 
 # build logs
 $ vue init pwa hellopj
+
+
+# 参考
+https://narinymous.hatenablog.com/entry/2018/03/12/004015
+
+https://kuroeveryday.blogspot.jp/2017/10/vue-2-5-with-typescript.html
+
+https://github.com/angular/angular-cli/issues/9783
+
+ ts-loader version ...
+
--- hellopj/build/webpack.base.conf.js
+++ hellopj/build/webpack.base.conf.js
@@ -11,7 +11,7 @@ function resolve (dir) {
 
 module.exports = {
   entry: {
-    app: './src/main.js'
+    app: './src/main.ts'
   },
   output: {
     path: config.build.assetsRoot,
@@ -21,7 +21,7 @@ module.exports = {
       : config.dev.assetsPublicPath
   },
   resolve: {
-    extensions: ['.js', '.vue', '.json'],
+    extensions: ['.js', '.ts', '.vue', '.json'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src')
@@ -42,6 +42,13 @@ module.exports = {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: vueLoaderConfig
+      },
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        options: {
+          appendTsSuffixTo: [/\.vue$/]
+        }
       },
       {
         test: /\.js$/,
